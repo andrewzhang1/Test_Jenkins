3e2f70dce353cbace2cd7ec603d528e44cdd4fd2
Andrew Zhang <agz1117@hotmail.com>
2017-03-12 12:50:50 -0700

    first commit

---
+++ send_result.sh
@@ -0,0 +1,10 @@
+# A script to send result to "agz1117@hotmail.com"
+# This script will find the last 
+ls -ltr > temp ; tail -l temp 
+
+export LAST_LOG_FILE="`tail -1 temp | cut -d" " -f10`"
+
+echo "Last log file to be sent is: $LAST_LOG_FILE"
+
+# mail attachement
+cat $LAST_LOG_FILE | mail agz1117@hotmail.com 
---
+++ testMain.sh
@@ -0,0 +1,13 @@
+# A main script to check the resource for the machine
+# Andrew Zhang 3/12/2017
+
+# The script will generate a output log file in the format like:
+# 170312110336.log
+
+export logF=$(date +%y%m%d%H%M%S).log
+
+echo -e "\nHello, $LOGNAME, cuurent time is `date`" 2>&1 | tee -a $logF
+echo -e "\nThe current resouce uase on the linux machine "`hostname`" is: " 2>&1 | tee -a $logF
+
+vmstat 2>&1 | tee -a $logF 
+
