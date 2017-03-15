e90227b17d29eb1861929baa8cf68f07f8086d26
Andrew Zhang <agz1117@hotmail.com>
2017-03-14 17:37:40 -0700

    Update send_result.sh

--- send_result.sh
+++ send_result.sh
@@ -9,4 +9,4 @@ export LAST_LOG_FILE="`tail -1 temp | cut -d" " -f10`"
 echo "Last log file to be sent is: $LAST_LOG_FILE"
 
 # mail attachement
-cat $LAST_LOG_FILE | mail agz1117@hotmail.com 
+# cat $LAST_LOG_FILE | mail agz1117@hotmail.com 
